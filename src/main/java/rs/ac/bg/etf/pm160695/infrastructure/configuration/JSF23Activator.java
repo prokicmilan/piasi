df014ef4fc673b4d07bb928de227175ee8db52c6
Milan Prokić <prokic.milan@gmail.com>
2020-01-04 13:28:36 +0100

    initial commit

---
+++ src/main/java/rs/ac/bg/etf/pm160695/infrastructure/configuration/JSF23Activator.java
@@ -0,0 +1,12 @@
+package rs.ac.bg.etf.pm160695.infrastructure.configuration;
+
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+/**
+ * Klasa koja obezbedjuje koriscenje JSF verzije 2.3
+ * @author Proka
+ *
+ */
+@FacesConfig(version = Version.JSF_2_3)
+public class JSF23Activator {}
